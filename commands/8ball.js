48c0dfbb1f80129a8ff3e03afbc36798afb984b9
Allen <103039975+NB112211@users.noreply.github.com>
2023-10-19 08:56:38 +0800

    更新 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,39 +1,39 @@
 const responses = [
-  "It is certain.",
-  "Without a doubt.",
-  "You may rely on it.",
-  "Yes, definitely.",
-  "It is decidedly so.",
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Signs point to yes.",
-  "Absolutely!",
-  "Don't count on it.",
-  "My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful.",
-  "Not likely.",
-  "Better not tell you now.",
-  "Chances are low.",
-  "Unlikely.",
-  "No way LMAO.",
-  "I highly doubt it.",
+  "确定无疑。",
+  "毫无疑问。",
+  "你可以依赖它。",
+  "是的，绝对如此。",
+  "果断地说，是的。",
+  "据我看，是的。",
+  "很有可能。",
+  "前景看好。",
+  "迹象表明是的。",
+  "绝对！",
+  "不要指望。",
+  "我的消息来源说不是。",
+  "前景不太好。",
+  "非常怀疑。",
+  "不太可能。",
+  "现在最好不要告诉你。",
+  "机会不大。",
+  "不太可能。",
+  "绝不，哈哈。",
+  "我非常怀疑。",
 ];
 
 module.exports = {
   name: "8ball",
-  description: "Ask the magic 8ball a question",
+  description: "向神奇的8球提出一个问题",
   alias: ["eightball"],
-  usage: "/8ball <question>",
-  example: "/8ball Is this bot awesome?",
-  category: "Fun",
+  usage: "/8ball <问题>",
+  example: "/8ball 这个机器人很棒吗？",
+  category: "娱乐",
   handler: async (ctx) => {
     const { message } = ctx;
     const { text } = message;
 
     if (!text.substring(text.indexOf(" ") + 1)) {
-      await ctx.reply("You need to ask a question!");
+      await ctx.reply("你需要提出一个问题！");
       return;
     }
 
